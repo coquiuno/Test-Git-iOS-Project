a885198db2d3b19cf160122770400daef17c3f64
Compu <Coquiuno@Caminante.local>
2014-05-05 11:31:48 -0400

    added a test class

---
+++ Git Test/XYZTest.h
@@ -0,0 +1,13 @@
+//
+//  XYZTest.h
+//  Git Test
+//
+//  Created by Compu on 5/5/14.
+//  Copyright (c) 2014 CyL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYZTest : NSObject
+
+@end
---
+++ Git Test/XYZTest.m
@@ -0,0 +1,13 @@
+//
+//  XYZTest.m
+//  Git Test
+//
+//  Created by Compu on 5/5/14.
+//  Copyright (c) 2014 CyL. All rights reserved.
+//
+
+#import "XYZTest.h"
+
+@implementation XYZTest
+
+@end
